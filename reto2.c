0b7e5f8c1bf5765d1976779499aae3d48cbb1cd0
Eduardo Reyes <eduarno.1@hotmail.com>
2020-04-16 19:44:07 -0600

    La comparacion de la logitud de las dos palabras funcona

--- reto2.c
+++ reto2.c
@@ -6,10 +6,21 @@ int main(int argc, char const *argv[])
 {
     int palabra1 = strlen(argv[1]);
     int palabra2 = strlen(argv[2]);
-    char * mostrar;
-    mostrar = "Hola"; 
-    printf("%s", mostrar);
+    if (palabra1 > palabra2)
+    {
+        printf("%s", argv[1]);
+    } else if (palabra1 < palabra2)
+    {
+        printf("%s", argv[2]);
+    } else if (palabra1 == palabra2)
+    {
+        printf("Empate");
+    } else
+    {
+        printf("Aqui hubo un error");
+    }
     return 0;
 }
+
 
 /*El reto es crear un programa que reciba cadenas de caracteres y diga cual es más grande*/
