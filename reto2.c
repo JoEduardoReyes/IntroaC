9924c93b589bfb8fc83ed84e73a847b17bad36e1
Eduardo Reyes <eduarno.1@hotmail.com>
2020-04-16 19:51:34 -0600

    Agregamos texto a los resultados y funciona programa terminado

--- reto2.c
+++ reto2.c
@@ -8,16 +8,13 @@ int main(int argc, char const *argv[])
     int palabra2 = strlen(argv[2]);
     if (palabra1 > palabra2)
     {
-        printf("%s", argv[1]);
+        printf("La palabra más grande es %s con %ld caracteres", argv[1], palabra1);
     } else if (palabra1 < palabra2)
     {
-        printf("%s", argv[2]);
+        printf("La palabra más grande es %s con %ld caracteres", argv[2], palabra2);
     } else if (palabra1 == palabra2)
     {
-        printf("Empate");
-    } else
-    {
-        printf("Aqui hubo un error");
+        printf("Empate, ambas palabras tienen %ld caracteres", palabra1);
     }
     return 0;
 }
